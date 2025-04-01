a543344aed5365429240bd500ab33d3853d5d329
UNIMEDCARUARU\allan.vieira <teste@teste.com.br>
2025-04-01 14:51:15 -0300

    primeiro commit

---
+++ src/main/java/com/i9/depreciation/DepreciationApplication.java
@@ -0,0 +1,13 @@
+package com.i9.depreciation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepreciationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepreciationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/i9/depreciation/DepreciationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i9.depreciation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepreciationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
